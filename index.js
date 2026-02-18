8f2ada51e705f2747fd6350300171fc20fa8af1c
Shaffaq Hai <hai.shaffaq@gmail.com>
2026-02-18 00:11:04 -0500

    added some graphql

--- index.js
+++ index.js
@@ -3,13 +3,61 @@ const mongoose = require('mongoose')
 require("dotenv").config()
 
 const { buildSchema } = require('graphql')
-import { createHandler } from 'graphql-http/lib/use/express';
+//import { createHandler } from 'graphql-http/lib/use/express';
 
 //Schema
+const gplSchema = buildSchema(`
+        type Query {
+            loginUser()
+            getAllEmployees: [Employee]
+            getEmployeeById(_id: ID!) : Employee
+            searchEmployee(
+            designation: String
+            department: String
+            ) : [Employee]
+        
+        
+        }
+
+        type Mutation {
+                signupUser(username: String!, password: String!, email: String!):User
+                addEmployee()
+                updateEmployee()
+                deleteEmployee()
+        
+        
+        
+        }
+        type User{
+            id: ID
+            username: String
+            password: String
+            email: String
+
+    
+        }
+        type Employee {
+            first_name: String
+            last_name: String
+            email: String
+            gender: String
+            designation: String
+            salary: Float
+
+
+        
 
 
 
+        }
 
+    `)
+
+
+
+const rootresolver = {
+
+}
 
 const app = express()
 const PORT = 4000
