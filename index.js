3cc4a3e3e9a62777df191fc03d942756d46b2fc9
Shaffaq Hai <hai.shaffaq@gmail.com>
2026-02-18 00:47:14 -0500

    completed boilerplate

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const mongoose = require('mongoose')
 require("dotenv").config()
 
 const { buildSchema } = require('graphql')
-//import { createHandler } from 'graphql-http/lib/use/express';
+const { createHandler } = require('graphql-http/lib/use/express')
 
+
+const app = express()
+const PORT = 4000
+
+const connectDB = async() => {
+    try{
+        console.log(`Attempting to connect to DB`);
+        const DB_CONNECTION = process.env.DB_CONNECTION
+        mongoose.connect(DB_CONNECTION).then( () => {
+            console.log(`MongoDB connected`)
+        }).catch( (err) => {
+            console.log(`Error while connecting to MongoDB : ${JSON.stringify(err)}`)
+        });
+    }catch(error){
+        console.log(`Unable to connect to DB : ${error.message}`);
+        
+    }
+}
 //Schema
-const gplSchema = buildSchema(`
+const schema = buildSchema(`
         type Query {
-            loginUser()
+            loginUser(username: String!, password: String!): User
             getAllEmployees: [Employee]
             getEmployeeById(_id: ID!) : Employee
             searchEmployee(
@@ -21,9 +39,28 @@ const gplSchema = buildSchema(`
 
         type Mutation {
                 signupUser(username: String!, password: String!, email: String!):User
-                addEmployee()
-                updateEmployee()
-                deleteEmployee()
+                addEmployee(
+                    first_name: String,
+                    last_name: String,
+                    email: String,
+                    gender: String,
+                    designation: String,
+                    salary: Float,
+                    date_of_joining: String,
+                    department: String,
+                ): User
+                updateEmployee(
+                    _id: ID!,
+                    first_name: String,
+                    last_name: String,
+                    email: String,
+                    gender: String,
+                    designation: String,
+                    salary: Float,
+                    date_of_joining: String,
+                    department: String
+                    ) : User
+                deleteEmployee(_id: ID!) : User
         
         
         
@@ -43,39 +80,28 @@ const gplSchema = buildSchema(`
             gender: String
             designation: String
             salary: Float
-
-
-        
-
-
-
+            date_of_joining: String
+            department: String
         }
 
     `)
 
 
 
-const rootresolver = {
+const root = {
 
 }
 
-const app = express()
-const PORT = 4000
 
-const connectDB = async() => {
-    try{
-        console.log(`Attempting to connect to DB`);
-        const DB_CONNECTION = process.env.DB_CONNECTION
-        mongoose.connect(DB_CONNECTION).then( () => {
-            console.log(`MongoDB connected`)
-        }).catch( (err) => {
-            console.log(`Error while connecting to MongoDB : ${JSON.stringify(err)}`)
-        });
-    }catch(error){
-        console.log(`Unable to connect to DB : ${error.message}`);
-        
-    }
-}
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  }),
+);
+
+
 
 app.listen(PORT, () =>{
     connectDB()
